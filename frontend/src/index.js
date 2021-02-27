d5de1b8af86c97291f2b84ba27f9bb8a8b78026e
Miah Barnes <miahellenbarnes@gmail.com>
2021-02-27 10:01:04 -0500

    redux skeleton

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import configureStore from './store';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+
+
+/*
+Create a variable to access your store and expose it to the window.
+It should not be exposed in production, be sure this is only set in development.
+*/
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
