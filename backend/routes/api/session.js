8264c485269e5aff71624a1478145ee3d63a0681
Miah Barnes <miahellenbarnes@gmail.com>
2021-02-27 08:11:09 -0500

    backend routes, signup login, logout,

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -6,21 +6,25 @@ const { User } = require('../../db/models');
 
 //login route, needs to be updated for your cradentials later;
 router.post('/', asyncHandler(async (req, res, next) => {
-        const { credential, password } = req.body;
-        console.log(credential, password);
-        const user = await User.login({ credential, password });
+    const { credential, password } = req.body;
+    console.log(credential, password);
+    const user = await User.login({ credential, password });
 
-        if (!user) {
-            const err = new Error('Login failed');
-            err.status = 401;
-            err.title = 'Login failed';
-            err.errors = ['Uh oh... Looks like your username or password did not match a user in our system.'];
-            return next(err);
-        }
-        await setTokenCookie(res, user);
+    if (!user) {
+        const err = new Error('Login failed');
+        err.status = 401;
+        err.title = 'Login failed';
+        err.errors = ['Uh oh... Looks like your username or password did not match a user in our system.'];
+        return next(err);
+    }
+    await setTokenCookie(res, user);
 
-        return res.json({ user });
-    }),
-);
+    return res.json({ user });
+}));
+
+router.delete('/', (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+});
 
 module.exports = router;
