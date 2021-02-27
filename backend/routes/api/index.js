eead9512e7d130c5ece4b56a3bdbe0123e41dcc7
Miah Barnes <miahellenbarnes@gmail.com>
2021-02-27 07:46:31 -0500

    skeleton

---
+++ backend/routes/api/index.js
@@ -0,0 +1,46 @@
+const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+import sessionRouter from './session';
+import userRouter from './users';
+
+
+router.use('/session', sessionRouter);
+router.use('/users', userRouter);
+
+
+router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      },
+    })
+  setTokenCookie(res, user);
+  return res.json({ user });
+}));
+
+
+// GET /api/restore-user
+const { restoreUser } = require('../../utils/auth.js');
+router.get(
+  '/restore-user',
+  restoreUser,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+
+const { requireAuth } = require('../../utils/auth.js');
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+
+
+module.exports = router;
