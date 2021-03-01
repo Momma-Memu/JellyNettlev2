aabf08343453c6fc272a115880bfe1722408af5d
Miah Barnes <miahellenbarnes@gmail.com>
2021-02-28 20:50:54 -0500

    modal fix

--- frontend/src/components/modals/LoginModal.js
+++ frontend/src/components/modals/LoginModal.js
@@ -1,25 +1,23 @@
-import { useEffect } from 'react';
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Login from '../loginComponents/Login';
 
 const LoginModal = () => {
     const [display, setDisplay] = useState(false);
-
+    const modalRef = useRef();
 
     const reveal = () => {
         setDisplay(true);
     }
-    console.log(display)
 
-    const hide = () => {
-        setDisplay(false);
+    const hide = (e) => {
+        if(e.target .classList.contains('modal-overlay')) setDisplay(false);
     }
 
     return (
-        <div onClick={hide}>
+        <div>
             <button onClick={reveal} className='login-nav-button'>Login</button>
             { display  ?
-                <div className='modal-overlay'>
+                <div className='modal-overlay' ref={modalRef} onClick={hide}>
                     <Login />
                 </div>
             : null }
