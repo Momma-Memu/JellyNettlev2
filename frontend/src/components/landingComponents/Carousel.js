79175e67e327efc546d673d95ccf05e6aa4cf5f0
Miah Barnes <miahellenbarnes@gmail.com>
2021-03-02 10:53:45 -0500

    poop

--- frontend/src/components/landingComponents/Carousel.js
+++ frontend/src/components/landingComponents/Carousel.js
@@ -4,6 +4,8 @@ const Carousel = () => {
     const [images, setImages] = useState([])
     const arrowRef1 = useRef();
     const arrowRef2 = useRef();
+    const imageRef1 = useRef();
+    const imageRef2 = useRef();
 
 
     const slideInArrows = () => {
@@ -27,19 +29,29 @@ const Carousel = () => {
 
     useEffect(() => {
         const startingImages = [
-            <div className='div-image1' key={1}>
+            <div className='div-image1' key={1} ref={imageRef1}>
                 <div className='image-banner'>Build your own teams.</div></div>,
-            <div className='div-image2' key={2} onMouseOver={slideInArrows} onMouseLeave={hideSlideArrows}>
+            <div className='div-image2' key={2} ref={imageRef2}>
                 <div className='image-banner2'>Find people on your system.</div></div>
         ]
         setImages(startingImages);
     }, [])
 
     const rotateRight = () => {
-            const copy = images.slice();
-        const removed = copy.shift();
-        copy.push(removed);
-        setImages(copy);
+        const image1 = imageRef1.current;
+        const image2 = imageRef2.current;
+        const amount1 = document.querySelector(".div-image1").clientWidth;
+        const amount2 = document.querySelector(".div-image2").clientWidth;
+        // image1.classList.add('div-image1-rotate-right')
+        // image2.classList.add('div-image2-rotate-right')
+        // setTimeout(() => {
+        //     const copy = images.slice();
+        //     const removed = copy.shift();
+        //     copy.push(removed);
+        //     setImages(copy);
+        //     image1.classList.remove('div-image1-rotate-right')
+        //     image2.classList.remove('div-image2-rotate-right')
+        // }, 1000);
 
     }
 
