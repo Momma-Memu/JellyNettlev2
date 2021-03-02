b930ed20bb2a17beeedfd56912c883be9f9133fd
Miah Barnes <miahellenbarnes@gmail.com>
2021-03-02 16:10:32 -0500

    refactoring carousel

--- frontend/src/components/landingComponents/Carousel.js
+++ frontend/src/components/landingComponents/Carousel.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState, useRef } from 'react'
 
 const Carousel = () => {
     const [images, setImages] = useState([])
+    const [position, setPosition] = useState(0)
     const arrowRef1 = useRef();
     const arrowRef2 = useRef();
     const imageRef1 = useRef();
     const imageRef2 = useRef();
+    const imageRef3 = useRef();
 
 
     const slideInArrows = () => {
@@ -34,7 +36,9 @@ const Carousel = () => {
             <div className='div-image1' key={1} ref={imageRef1}>
                 <div className='image-banner'>Build your own teams.</div></div>,
             <div className='div-image2' key={2} ref={imageRef2}>
-                <div className='image-banner2'>Join groups related to your interests.</div></div>
+                <div className='image-banner2'>Join groups related to your interests.</div></div>,
+            <div className='div-image3' key={3} ref={imageRef3}>
+                <div className='image-banner2'>Create a commmunity for your system.</div></div>
         ]
         setImages(startingImages);
     }, [])
@@ -42,22 +46,34 @@ const Carousel = () => {
     const rotateRight = () => {
         const image1 = imageRef1.current;
         const image2 = imageRef2.current;
-        image1.classList.add('div-image1-rotate-right')
-        image2.classList.add('div-image2-rotate-right')
-        setTimeout(() => {
-            image2.classList.remove('div-image2-rotate-right')
-            image1.classList.remove('div-image1-rotate-right')
-            const copy = images.slice();
-            const removed = copy.shift();
-            copy.push(removed);
-            setImages(copy);
-        }, 1000);
+        const image3 = imageRef3.current;
+
+        [image1, image2, image3].forEach((el) => {
+            const left = el.style.left;
+            if(!left || left === '0px') el.style.left = '-100%';
+            if(left === '-100%') el.style.left = '-200%'
+            if(left === '-200%') el.style.left = '0px'
+        })
+
+    }
+
+    const rotateLeft = () => {
+        const image1 = imageRef1.current;
+        const image2 = imageRef2.current;
+        const image3 = imageRef3.current;
+
+        [image1, image2, image3].forEach((el) => {
+            const left = el.style.left;
+            if(!left || left === '0px') el.style.left = '-200%';
+            if(left === '-100%') el.style.left = '0px';
+            if(left === '-200%') el.style.left = '-100%';
+        })
 
     }
 
     return (
         <div className='carousel-wrapper' onMouseOver={slideInArrows} onMouseLeave={hideSlideArrows}>
-            <i className="far fa-caret-square-right prev-button-hidden" ref={arrowRef2}></i>
+            <i className="far fa-caret-square-right prev-button-hidden" ref={arrowRef2} onClick={rotateLeft}></i>
                 <div className='carousel-container'>
                     {images}
                 </div>
