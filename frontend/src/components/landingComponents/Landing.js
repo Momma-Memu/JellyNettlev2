2e02ba33be9d73b26efa7fe2384079fb0cea8f75
Miah Barnes <miahellenbarnes@gmail.com>
2021-03-03 11:14:19 -0500

    plan-events

--- frontend/src/components/landingComponents/Landing.js
+++ frontend/src/components/landingComponents/Landing.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Carousel from './Carousel';
+import PlanEvents from './PlanEvents';
 
 const Landing = () => {
 
     return (
-        <Carousel />
+        <>
+            <Carousel />
+            <PlanEvents />
+        </>
     )
 }
 
---
+++ frontend/src/components/landingComponents/PlanEvents.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const PlanEvents = () => {
+
+
+    return (
+        <div className='plan-events-container'>
+            <div className='plan-events-header'>Plan Events</div>
+            <i className="fas fa-sitemap event-icon"></i>
+        </div>
+    )
+}
+
+export default PlanEvents;
