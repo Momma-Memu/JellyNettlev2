940d7b2a9030fdbf60f85498fe9c8bf9dae9d79a
Miah Barnes <miahellenbarnes@gmail.com>
2021-03-17 08:58:00 -0400

    color changes

--- frontend/src/components/NavBarComponents/NavBar.js
+++ frontend/src/components/NavBarComponents/NavBar.js
@@ -2,16 +2,16 @@ import LoginModal from '../modals/LoginModal';
 import { useSelector } from 'react-redux';
 import Logout from './Logout';
 import SignUpModal from '../modals/SignUpModal';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     const user = useSelector(state => state.session.user);
 
     return (
         <div className='navigation-bar'>
-            <NavLink to='/'>
+            <Link to='/'>
                 <div className='navbar-logo' />
-            </NavLink>
+            </Link>
             <div className='website-title'>JellyNettle</div>
             {user === null ? <SignUpModal /> : null}
             {user === null ? <LoginModal /> : <Logout />}
