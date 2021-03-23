46ed9e56bbd84333af6a317c010f93a9269be370
Miah Barnes <miahellenbarnes@gmail.com>
2021-03-23 19:09:18 -0400

    footer version 2

--- frontend/src/components/landingComponents/Footer.js
+++ frontend/src/components/landingComponents/Footer.js
@@ -3,11 +3,7 @@ const Footer = () => {
     return (
         <div className='footer-box'>
             <div className='footer-header-box'>
-                <h1 className='footer-header-links'>Creater Links</h1>
-                <a href='https://miahbarnes.com'>
-                    <i className="fas fa-save icon"></i>
-                    Portfolio
-                </a>
+                <h1 className='footer-header-links'>Contact Me</h1>
                 <a href='https://github.com/OnionQueenMemu/JellyNettlev2'>
                     <i className="fab fa-github-square icon"></i>
                     Github Repo
@@ -16,6 +12,35 @@ const Footer = () => {
                     <i className="fab fa-linkedin icon"></i>
                     LinkedIn
                 </a>
+                <a href='https://miahbarnes.com'>
+                    <i className="fas fa-save icon"></i>
+                    Portfolio
+                </a>
+                <a href='https://twitter.com/OnionQueenMemu'>
+                    <i className="fab fa-twitter-square icon"></i>
+                    Twitter
+                </a>
+            </div>
+            <div className='footer-header-box'>
+                <h1 className='footer-header-links'>Technologies</h1>
+                <div className='footer-tech-box'>
+                    <div className='footer-list-box'>
+                        <span className='list-label'>Backend</span>
+                        <span className='list-item'>JavaScript</span>
+                        <span className='list-item'>Node</span>
+                        <span className='list-item'>Express</span>
+                        <span className='list-item'>Sequelize</span>
+                        <span className='list-item'>PostgreSQL</span>
+                    </div>
+                    <div className='footer-list-box'>
+                        <span className='list-label'>Frontend</span>
+                        <span className='list-item'>JavaScript</span>
+                        <span className='list-item'>React</span>
+                        <span className='list-item'>Redux</span>
+                        <span className='list-item'>CSS</span>
+                        <span className='list-item'>HTML</span>
+                    </div>
+                </div>
             </div>
         </div>
     )
