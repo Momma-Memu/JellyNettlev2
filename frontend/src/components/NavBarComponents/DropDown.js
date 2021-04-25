e57293d062fa8c1652c9a28e0c90461dfa1c9701
Miah Barnes <miahellenbarnes@gmail.com>
2021-04-25 13:10:09 -0400

    drop down reworking again

--- frontend/src/components/NavBarComponents/DropDown.js
+++ frontend/src/components/NavBarComponents/DropDown.js
@@ -16,12 +16,12 @@ const DropDown = (props) => {
     const openMenu = () => {
         let showing;
         if(menuRef) {
-            showing = menuRef.current.style.height;
-            if(!showing || showing === '0px'){
-                menuRef.current.style.height = '400px';
+            showing = menuRef.current.style.top;
+            if(!showing || showing === '-260%'){
+                menuRef.current.style.top = '260%';
                 menuRef.current.style.boxShadow = '4px 3px 3px 1px rgba(148, 148, 148, 0.308)';
             } else {
-                menuRef.current.style.height = '0px'
+                menuRef.current.style.top = '-260%'
                 menuRef.current.style.boxShadow = 'none';
             }
         };   
