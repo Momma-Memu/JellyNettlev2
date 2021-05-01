eb03f44aff73f1b1ecdd831da5bafb6a7df8b66e
Miah Barnes <miahellenbarnes@gmail.com>
2021-05-01 15:12:41 -0400

    profile component

--- frontend/src/components/profileComponents/Profile.js
+++ frontend/src/components/profileComponents/Profile.js
@@ -1,12 +1,36 @@
 import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 const Profile = () => {
-    const user = useSelector(state => state.session.user)
+    const { username } = useParams()
+    const user = useSelector(state => state.session.user);
+    const profile = useSelector(state => state.profile);
+    const privacy = useSelector(state => state.privacy);
 
-    return (
-        <div>hello bitch</div>
-    )
+    if(!user || !profile || !privacy) return null;
 
-}
+    if(username === user.username){
+        return (
+            <>
+                <div className='profile-header'>
+                    <img src={profile.photoUrl} className='profile-picture' />
+                    <span className='profile-username'>{username}</span>
+                    <span>{profile.introduction}</span>
+                </div>
+                <div className='profile-body'>
+                    <div className='profile-details'>
+                        <span>System: {profile.favoriteConsole}</span>
+                        {privacy.gender ?< span>Gender: {profile.gender}</span> : null}
+                        {privacy.dob ? <span>Birthday: {profile.dob}</span> : null}
+                        <span>Member Since: {user.createdAt.split('-')[0]}</span>
+                    </div>
+                </div>
+            </>
+        );
+    } else {
+        return null;
+    }
+
+};
 
 export default Profile;
