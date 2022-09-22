ec4fd864e1a647167c05d09a14f133391c36add5
Miah Barnes <miahellenbarnes@gmail.com>
2022-09-22 17:41:44 -0400

    add styling for default images in notifications

--- frontend/src/components/NavBarComponents/Notifications.js
+++ frontend/src/components/NavBarComponents/Notifications.js
@@ -8,9 +8,22 @@ const Notifications = ({props}) => {
     return (
         <div className="modal-point">
             <div className="modal-container-top" ref={menuRef}>
-                {notifications.map(notification => {
+                {notifications.map((notification, i) => {
                     return (
-                        <div>{notification.fromUsername}</div>
+                        <div className={`${i % 2 !== 0 ? 'notification-container' : 'notification-container-light'} pt-2 pb-2`}>
+                            <div className="flex-horizon flex-align-center">
+                                {
+                                    notification.fromUserProfilePicture ? <img className="h-40 radius-20 ml-2 mr-2" src={notification.fromUserProfilePicture}></img> :
+                                    <i className="fa fa-user search-results-default-user-icon ml-2 mr-2" aria-hidden="true"></i>
+                                }
+                                <div className="fsize-1">Friend request from, {notification.fromUsername}.</div>
+                                <div> </div>
+                            </div>
+                            <div className="flex-horizon flex-align-center mt-2">
+                                <div className="choice-button-light">Confirm</div>
+                                <div className="choice-button-light">Decline</div>
+                            </div>
+                        </div>
                     )
                 })}
             </div>
